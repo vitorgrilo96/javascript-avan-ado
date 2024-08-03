9039ab095d5652758b8b9198bc85b0b9de5a1522
instrutorvilsonmoro@gmail.com <instrutorvilsonmoro@gmail.com>
2024-08-02 22:01:00 -0300

    metade desta atividade foi feita

--- requisicoes/estados.js
+++ requisicoes/estados.js
@@ -14,18 +14,85 @@ function conseguirTudo() {
     fetch("http://localhost:3000/estados", {
         method: "get"
     })
-    .then(resposta => resposta.json())
-    .then(dados => {
-       for (let i = 0; i < dados.length; i++) {
-        let estado = dados[i];
-        console.log(estado.nome, estado.sigla);
-       }
+        .then(resposta => resposta.json())
+        .then(dados => {
+            for (let i = 0; i < dados.length; i++) {
+                let estado = dados[i];
+                console.log(estado.nome, estado.sigla);
+            }
+        })
+        .catch(erro => {
+            console.error("algo de errado não está certo");
+        })
+}
+
+async function obterEstadosComNome(nome) {
+    let estados = await conseguirTudo();
+    if (estados === null)
+        return null;
+
+    for (let i = 0; i < estados.length; i++) {
+        let estado = estados[i];
+        if (estado.nome === nome)
+            return estado;
+    }
+}
+
+function deletarComId(id) {
+    fetch(`http://localhost:3000/estados/${id}`, {
+        method: "delete"
     })
-    .catch(erro => {
-        console.error("algo de errado não está certo");
+        .then(requisicao => {
+            if (requisicao.ok)
+                console.log("aviso: você acabou de apagar o estado");
+            else
+                console.log(`este id está incorreto ${id}`);
+        })
+        .catch(erro => console.error("erro ao deletar"));
+}
+
+function mudar(id, nome, sigla) {
+    let estado = {
+        "nome": nome,
+        "sigla": sigla 
+    }
+    fetch(`http://localhost:3000/estados/${id}`, {
+        method: "put",
+        body: JSON.stringify(estado, sigla)
     })
+        .then(response => {
+            if (response.ok === false) {
+                console.log(`id desconhecido ${id}`);
+                return;
+            }
+            console.log("estado alterado");
+        })
+        .catch(error => console.error("não foi possivel alterar o estado"));
+}
+
+async function apagarComNome(nome) {
+    let estado = await obterEstadosComNome(nome);
+    if (estado === null) {
+        console.log(`este estado não existe ${nome}`);
+        return;
+    }
+    deletarComId(estado.id);
+}
+
+async function mostrarTudo() {
+    let estados = await conseguirTudo();
+    console.log("estados");
+
+    for (let index = 0; index < estados.length; index++) {
+        const estado = estados[index];
+        console.log(`id: ${estado.id} nome: ${estado.nome}`)
+    }
 }
 
 /*cadastrar("rio grande do sul", "RS");
 cadastrar("paraná", "PR");*/
+//cadastrar("narnia", "NA");
+//mudar("c985", "são paulo", "SP");
+//mudar("f361", "minas gerais", "MG")
+//deletarComId("c23b")
 conseguirTudo();
--- requisicoes/marcas.js
+++ requisicoes/marcas.js
@@ -18,26 +18,86 @@ function cadastrar(nomeMarca) {
     .catch(erro => console.error("erro ao tentar cadastrar a marca"));
 }
 
-function obterTodos() {
-    fetch("http://localhost:3000/marcas", {
+async function obterTodos() {
+    let resposta = await fetch("http://localhost:3000/marcas", {
         method: "get"
-    })
-    .then(resposta => resposta.json())
-    .then(dados => {
-       for (let i = 0; i < dados.length; i++) {
-        let marca = dados[i];
-        console.log(marca.nome);
-       }
-    })
-    .catch(erro => {
+    });
+
+    if(resposta.ok){
+        let marcas = resposta.json();
+        return marcas;
+    }
+
         console.error("ocorreu um erro ao consultar as marcas");
-    })
+        return null;
 }
 
-function alterar() {}
+async function obterMarcasPorNome(nome){
+    let marcas = await obterTodos();
+    if (marcas === null)
+        return null;
 
-function apagar() {}
+        for (let i = 0; i < marcas.length; i++){
+            let marca = marcas[i];
+            if(marca.nome === nome)
+                return marca;
+        }
+}
 
-cadastrar("razer");
+function apagarPorId(id){
+    fetch(`http://localhost:3000/marcas/${id}`,{
+        method: "delete"
+    })
+    .then(requisicao => {
+        if (requisicao.ok)
+            console.log("marca apagada com sucesso");
+        else
+            console.log(`não encontrada marca com o id ${id}`);
+    })
+    .catch(erro => console.error("não foi possivel apagar"));
+}
+
+function alterar(id, nome) {
+    let marca = {
+        "nome": nome
+    }
+    fetch(`http://localhost:3000/marcas/${id}`, {
+       method: "put",
+       body: JSON.stringify(marca)
+    })
+    .then(response => {
+        if (response.ok === false){
+            console.log(`marca não alterada pois não foi possivel encontrar com o id ${id}`);
+            return;
+        }
+        console.log("marca alterada com sucesso");
+    })
+    .catch(error => console.error("não foi possivel alterar a marca"));
+}
+
+async function apagarPorNome(nome) {
+    let marca = await obterMarcasPorNome(nome);
+    if (marca === null){
+        console.log(`não encontrada marca com o nome ${nome}`);
+        return;
+    }
+    apagarPorId(marca.id);
+}
+
+async function aprensentarTodos() {
+    let marcas = await obterTodos();
+    console.log("marcas");
+
+    for (let index = 0; index < marcas.length; index++) {
+        const marca = marcas[index];
+        console.log(`id: ${marca.id} nome: ${marca.nome}`)
+    }
+}
+
+//apagarPorNome("razer");
+//apagarPorId("9de9")
+//function apagar() {}
+//cadastrar("razer");
+alterar("1", "asus");
 
 obterTodos();
